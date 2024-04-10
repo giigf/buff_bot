294cf430c7b95564ab08c22a2f38974cdd9ae86b
giigf <leshaburunday@gmail.com>
2024-04-11 01:35:13 +0600

    ну как бы да

--- 123.py
+++ 123.py
@@ -1,7 +1,17 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import requests
+import pandas as pd
 
 myurl = "https://buff.163.com/goods/33960"
-
 zapros = requests.get(myurl)
-print(zapros.content)
+
+
+#этофункция парсинга
+soup = bs(zapros.text, "html.parser")
+
+#float_ak = soup.find('span', class_='cru-market')   принимает тег и класс и ищет его
+ 
+#for ak in float_ak:
+ #   print(ak.tr['class']) по логике в будущем должно так работать чтоб выводить все калаши с этим флотом
+
+
