e991559f4ea816b77eb1fd07f45f0caec885e5ba
giigf <leshaburunday@gmail.com>
2024-04-12 22:53:20 +0600

    выводит флоты и меняет страницы

--- ClassBot.py
+++ ClassBot.py
@@ -3,3 +3,6 @@ class Skin:
     def __init__(self, float_, price):
         self.float_ = float_    
         self.price = price        
+    
+    def print_(self):
+        print(self.float_, "   ", self.price)
