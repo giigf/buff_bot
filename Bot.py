1cd804e99882fd55d79b563b69e228e92cb3a223
giigf <leshaburunday@gmail.com>
2024-04-13 23:46:31 +0600

    bot0.2

--- Bot.py
+++ Bot.py
@@ -18,7 +18,7 @@ page_num = 1
 while True:
     for MFS in MaxFloatSkin:
         while True:
-            driver.get(f'https://buff.163.com/goods/{MFS.Skin}?from=market#tab=selling&min_paintwear=0.00&max_paintwear={MFS.Maxfloat}&page_num={page_num}&max_price={MFS.Price}')
+            driver.get(f'https://buff.163.com/goods/{MFS.Skin}?from=market#tab=selling&min_paintwear=0.00&max_paintwear={MFS.Maxfloat}&page_num={1}&max_price={MFS.Price}')
            
             time.sleep(10)   
 
@@ -35,26 +35,24 @@ while True:
                         data = ''.join(re.findall(r'[\d\.-]',row.find('div', class_='wear-value').text))
                     if row.find('strong', class_='f_Strong') is not None:
                         price_ = ''.join(re.findall(r'[\d\.-]',row.find('strong', class_='f_Strong').text))
+                        Skin_.append(ClassBot.Skin(data, price_))     
                         
-                        
-                    Skin_.append(ClassBot.Skin(data, price_))     
-                    driver.find_element("link text", 'Купить').click()
-                           
-                    time.sleep(3)
-                    if driver.find_element("link text", 'Оплатить').click() == True:
+                        driver.find_element("link text", 'Купить').click()
+                        time.sleep(3)
                         driver.find_element("link text", 'Оплатить').click()
                         time.sleep(3)
                         driver.find_element("link text", 'Отправить предложение в приложение').click()
+                        
+                    
+                           
+             
+                    
+                    
                               
-                    else: 
-                        continue
+                    
                          
                 
-                if page_num < 2:
-                    page_num += 1
-                else: 
-                    page_num = 1
-                    break
+               
                 for data in Skin_:
                     data.print_()
     
